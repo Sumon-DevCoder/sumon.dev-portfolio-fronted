06874cd980ac53f7cfb9a98a440a9bfc5c54b5ed
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-11-20 17:53:45 +0600

    technical skills section completed

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import TechnicalSkills from "../components/TechnicalSkills";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <TechnicalSkills />
     </div>
   );
 };
